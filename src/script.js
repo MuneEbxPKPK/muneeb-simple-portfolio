5b31a0e4952014dee194446abcd8b63580ca24da
Muneeb <106381285+MuneEbxPKPK@users.noreply.github.com>
2024-08-05 10:49:55 +0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,14 +6,18 @@ fetch('https://api.ipify.org?format=json')
         let key = data.ip
         console.log(key);
 
-        Email.send({
-            Host: "smtp.gmail.com",
-            Username: "muneeb123karim@gmail.com",
-            Password: "y|UOVduh9^EF0dac",
-            To: 'muneeb323karim@gmail.com',
-            From: "muneeb123karim@gmail.com",
-            Subject: "From Simple Portfolio of Muneeb",
-            Body: "This is the Key: " + key
-        });
+       (function() {
+            // https://dashboard.emailjs.com/admin/account
+            emailjs.init({
+              publicKey: "MelJkzloYZExDjVTj",
+            });
+        })();
+
+                emailjs.sendForm('service_p2for1r', 'template_98w8bve', this)
+                    .then(() => {
+                        console.log('SUCCESS!');
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
 
     });
